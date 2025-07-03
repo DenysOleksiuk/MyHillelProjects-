f5f650a4af7c41febb2c72a11c0b2fa7b693ec00
Denys Oleksiuk <deoleksyk67@gmail.com>
2025-07-03 17:00:03 +0200

    add description

---
+++ src/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+public class Main {
+
+    private static double CONV_T = 273.15;
+
+
+    public static void main(String[] args) {
+        System.out.println("App for converting Celsius to Kelvin");
+        System.out.println("Version 1.0");
+
+
+    }
+}
