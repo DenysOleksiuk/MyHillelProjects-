fb8115913adc7534545696dc9d9fed782c75f396
Denys Oleksiuk <deoleksyk67@gmail.com>
2025-07-03 17:12:41 +0200

    add develop functional

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,12 +2,20 @@ package app;
 
 public class Main {
 
-
+    private static final double CONV_T = 273.15;
 
     public static void main(String[] args) {
         System.out.println("App for converting Celsius to Kelvin");
         System.out.println("Version 1.0");
 
 
+        double celsius = 25.0;
+        double kelvin = toKelvin(celsius);
+        System.out.println("Result is: " + "Kelvin: " + kelvin);
+
+    }
+
+    private static double toKelvin(double celsius) {
+        return celsius + CONV_T;
     }
 }
