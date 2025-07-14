3ba815b56fc6b5ed9580e900fe2ce926cbaf4914
Denys Oleksiuk <deoleksyk67@gmail.com>
2025-07-14 20:07:57 +0200

    fix classes

---
+++ src/Project_07/app/DataProvider.java
@@ -0,0 +1,8 @@
+package Project_07.app;
+
+public class DataProvider {
+
+    public String[] getData() {
+        return new String[]{"brange", "plum", "tomato", "onibn", "grape"};
+    }
+}
---
+++ src/Project_07/app/Main.java
@@ -0,0 +1,17 @@
+package Project_07.app;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        getOutput(new Corrector().handleData(
+                new DataProvider().getData())
+        );
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
+
