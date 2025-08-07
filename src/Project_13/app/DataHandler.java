8902d7b8855c321a06cff563bd1370b5d9b44912
Denys Oleksiuk <deoleksyk67@gmail.com>
2025-08-07 18:02:17 +0200

    fix classes

---
+++ src/Project_13/app/DataHandler.java
@@ -0,0 +1,18 @@
+package Project_13.app;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DataHandler {
+
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public synchronized int modify(int num) {
+        lock.lock();
+        try {
+            num = num * 3;
+            return num;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
